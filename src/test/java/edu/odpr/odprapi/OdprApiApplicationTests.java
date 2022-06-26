24872d792ee0b707e0a545805d085a5e6182ae76
tomas-quinonez <tomaslq98@gmail.com>
2022-06-25 21:49:54 -0300

    Initial commit

---
+++ src/test/java/edu/odpr/odprapi/OdprApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.odpr.odprapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdprApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
