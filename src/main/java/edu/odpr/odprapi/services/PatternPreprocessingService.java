8c363ef3e3562ad4ba73bc745613df707f8eac2e
tomas-quinonez <tomaslq98@gmail.com>
2022-12-18 16:15:44 -0300

    Solved problem with SubClassOf axioms

--- src/main/java/edu/odpr/odprapi/services/PatternPreprocessingService.java
+++ src/main/java/edu/odpr/odprapi/services/PatternPreprocessingService.java
@@ -14,18 +14,18 @@ import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.springframework.stereotype.Service;
 
 import edu.odpr.odprapi.model.Pattern;
-import edu.odpr.odprapi.utils.XMLManager.AxiomsXMLManager;
-import edu.odpr.odprapi.utils.XMLManager.ClassesXMLManager;
-import edu.odpr.odprapi.utils.XMLManager.OPsXMLManager;
+import edu.odpr.odprapi.utils.XMLManager.AxiomsParser;
+import edu.odpr.odprapi.utils.XMLManager.ClassesParser;
+import edu.odpr.odprapi.utils.XMLManager.OPsParser;
 
 @Service
 public class PatternPreprocessingService {
 
     public Pattern processPattern(InputStream inputStream) throws OWLOntologyCreationException {
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        ClassesXMLManager classNamesXMLManager = new ClassesXMLManager();
-        OPsXMLManager objectPropNamesXMLManager = new OPsXMLManager();
-        AxiomsXMLManager axiomsXMLManager = new AxiomsXMLManager();
+        ClassesParser classNamesXMLManager = new ClassesParser();
+        OPsParser objectPropNamesXMLManager = new OPsParser();
+        AxiomsParser axiomsXMLManager = new AxiomsParser();
         Pattern p = null;
         OWLOntology o = manager.loadOntologyFromOntologyDocument(inputStream);
         // IRI iri = o.getOntologyID().getOntologyIRI().get();
@@ -36,7 +36,6 @@ public class PatternPreprocessingService {
             String className = owlClass.getIRI().getShortForm();
             classNamesXMLManager.addIsClassSatisfiableQuery(className);
         }
-        
 
         Set<OWLObjectProperty> objectProperties = o.getObjectPropertiesInSignature();
         int cantOPs = objectProperties.size();
@@ -44,32 +43,40 @@ public class PatternPreprocessingService {
             String oPName = owlOP.getIRI().getShortForm();
             objectPropNamesXMLManager.addIsOPSatisfiableQuery(oPName);
         }
-        
 
         int cantAxioms = 0;
         Set<OWLAxiom> axioms = o.getAxioms();
         for (OWLAxiom owlAxiom : axioms) {
             Object[] classesArr = owlAxiom.getClassesInSignature().toArray();
             Object[] objectPropertiesArr = owlAxiom.getObjectPropertiesInSignature().toArray();
+            // Object[] e = owlAxiom.getNestedClassExpressions().toArray();
             String class1Name;
             String class2Name;
             String oPName;
 
             switch (owlAxiom.getAxiomType().getName()) {
                 case "SubClassOf":
-                    Object[] classExpressionsArr = owlAxiom.getNestedClassExpressions().toArray();
-                    OWLClassExpression firstExp = (OWLClassExpression) classExpressionsArr[0];
-                    class1Name = ((OWLClass) classesArr[0]).getIRI().getShortForm();
-                    class2Name = ((OWLClass) classesArr[1]).getIRI().getShortForm();
-                    if (firstExp.getClassExpressionType().toString().equals("ObjectSomeValuesFrom")) {
-                        oPName = ((OWLObjectProperty) objectPropertiesArr[0]).getIRI().getShortForm();
-                        axiomsXMLManager.addSomeValuesFromQuery(class1Name, class2Name, oPName);
-                        axiomsXMLManager.addOPDomainQuery(oPName, class1Name);
-                        axiomsXMLManager.addOPRangeQuery(oPName, class2Name);
-                    } else {
-                        axiomsXMLManager.addSubClassOfQuery(class1Name, class2Name);
+                    if (classesArr.length > 1) {
+                        Object[] classExpressionsArr = owlAxiom.getNestedClassExpressions().toArray();
+                        OWLClassExpression firstExp = (OWLClassExpression) classExpressionsArr[0];
+                        class1Name = ((OWLClass) classesArr[0]).getIRI().getShortForm();
+                        System.out.println(class1Name);
+                        if (objectPropertiesArr.length > 0) {
+                            System.out.println("relacion: "
+                                    + ((OWLObjectProperty) objectPropertiesArr[0]).getIRI().getShortForm());
+                        }
+                        class2Name = ((OWLClass) classesArr[1]).getIRI().getShortForm();
+
+                        if (firstExp.getClassExpressionType().toString().equals("ObjectSomeValuesFrom")) {
+                            oPName = ((OWLObjectProperty) objectPropertiesArr[0]).getIRI().getShortForm();
+                            axiomsXMLManager.addSomeValuesFromQuery(class1Name, class2Name, oPName);
+                            axiomsXMLManager.addOPDomainQuery(oPName, class1Name);
+                            axiomsXMLManager.addOPRangeQuery(oPName, class2Name);
+                        } else {
+                            axiomsXMLManager.addSubClassOfQuery(class1Name, class2Name);
+                        }
+                        cantAxioms++;
                     }
-                    cantAxioms++;
                     break;
 
                 case "ObjectPropertyDomain":
@@ -91,11 +98,11 @@ public class PatternPreprocessingService {
             }
 
         }
-        
+
         String classNamesXMLString = classNamesXMLManager.getXMLString();
         String opNamesXMLString = objectPropNamesXMLManager.getXMLString();
         String axiomsXMLString = axiomsXMLManager.getXMLString();
-        
+
         p = new Pattern(classNamesXMLString, opNamesXMLString, axiomsXMLString, cantClasses, cantOPs, cantAxioms);
         return p;
     }
